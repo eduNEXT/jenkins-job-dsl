6ecd3cb5d865cd095290981146affec2476f7e04
jinder1s <msingh@edx.org>
2020-03-25 15:08:10 -0400

    Modified script to work on jenkins

--- testeng/resources/create-repo-health-report.sh
+++ testeng/resources/create-repo-health-report.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
-set -e
+set -e -v
+
+# click requires this to work cause it interfaces weirdly with python 3 ASCII default
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 
-virtualenv --python=/usr/bin/python3.5 venv -q
+virtualenv --python=/usr/bin/python3.6 venv -q
 source venv/bin/activate
+
+ls
 
 cd pytest-repo-health
 make requirements
 pip install -e .
-pytest --repo-health --repo-health-path ../edx-repo-health --repo-path ../target_repo
+deactivate
+source ../venv/bin/activate
+
+touch tmp.txt
+pytest --repo-health --repo-health-path ../edx-repo-health --repo-path ../target_repo -c tmp.txt --noconftest
